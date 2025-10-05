3b75167bd9e1e3ce0f6235fab4bac424f357b7a6
Home Computer <rowpri0809@highgateschool.org.uk>
2025-10-05 12:24:41 +0100

    HW

--- class_excersises/OOP/dog oop.py
+++ class_excersises/OOP/dog oop.py
@@ -1,12 +1,18 @@
 # OOP exercises
 
 class Dog:
+
+    species = "Canis familiaris"
+
     def __init__(self,name,age):
         self.name = name
         self.age = age
 
-    def speaks(self):
-        return f'{self.name} says "woof", "woof"'
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
 
 
 rufus = Dog('Rufus',67)
